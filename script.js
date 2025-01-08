ec3539c1b40c5035731d54679716697884797f9b
CarlosVicenteB <carlosnulfovicente@gmail.com>
2025-01-08 15:29:53 -0500

    Manejando los botones y la pantalla de la calculadora

--- script.js
+++ script.js
@@ -7,8 +7,55 @@ const botones  = [
   "0", ".", "=", "/"
 ]
 
-const combinaciones = () => {
-  // Hace calculo y actualiza la pantalla
+const calculo = (op) => {
+  console.log(op)
+}
+
+
+// Manejamos las operaciones realizadas por los botones
+let operacion = '0'
+// agregar un regex 
+let bandera = true
+
+const acciones = ({ target }) => {
+  const pantalla = document.querySelector('.valor-pantalla')
+
+  const { textContent } = target
+
+  switch (textContent) {
+    case '=':
+      calculo(operacion)
+      break
+    case 'C':
+      if (operacion.length > 1) operacion = operacion.slice(0, -1)
+      break 
+    case 'AC':
+      operacion = '0'
+      break
+    case 'OF':
+      bandera = false
+      operacion = ''
+      break
+    case 'ON':
+      bandera = true
+      operacion = '0'
+      break
+    default:
+      const nuevaOperacion = operacion + textContent
+
+      if ( operacion === '0' && textContent !== '.') {
+        operacion = textContent
+      } else {
+        operacion = nuevaOperacion
+      }
+      break
+  }
+
+  if (bandera) {
+    pantalla.textContent = operacion
+  } else {
+    pantalla.textContent = ''
+  }
 }
 
 // Obtenemos el contenedor de los botones
@@ -19,6 +66,6 @@ botones.forEach( (value, id, _) => {
   const boton = document.createElement('button')
   boton.textContent = value
   boton.classList = `botones ${id}`
-  boton.addEventListener('click', combinaciones)
+  boton.addEventListener('click', acciones)
   contenedorBotones.appendChild(boton)
 })
